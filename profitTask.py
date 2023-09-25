a0e3b7cd8e0dbb97692006b84ad94f9ede84310e
samuelm-LCWC <138508687+samuelm-LCWC@users.noreply.github.com>
2023-09-25 12:53:04 +0100

    Update profitTask.py

--- profitTask.py
+++ profitTask.py
@@ -1,17 +1,17 @@
-product1({
+product1 = {
     "cost_price": 32.67,
     "sell_price": 45.00,
     "inventory": 1200
-})
+}
 
-product2({
+product2 = {
     "cost_price": 225.89,
     "sell_price": 550.00,
     "inventory": 100
-})
+}
 
-product3({
+product3 = {
     "cost_price": 2.77,
     "sell_price": 7.95,
     "inventory": 8500
-})
+}
